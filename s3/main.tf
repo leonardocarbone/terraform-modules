ea9b595b6e269c1df9f8bd651571ccbbabfe3c10
Leonardo Carbone <leonardoz.carbone@gmail.com>
2020-03-11 06:35:47 +0000

    Changed resource name

--- s3/main.tf
+++ s3/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "this" {
   bucket              = var.bucket_name
   region              = var.bucket_region
   acceleration_status = var.acceleration_status
--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_arn" {
   description = "ARN of the bucket"
-  value       = aws_s3_bucket.s3_bucket.arn
+  value       = aws_s3_bucket.this.arn
 }
 
 output "bucket_name" {
   description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.s3_bucket.id
+  value       = aws_s3_bucket.this.id
 }
